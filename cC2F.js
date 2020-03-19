85b3ffb30900406537aa91b01d96f8e21b76c081
Nisrine Jednane <34342007+NisrineJednane@users.noreply.github.com>
2020-03-19 16:35:32 -0700

    Create cC2F.js

---
+++ cC2F.js
@@ -0,0 +1,8 @@
+function convertToF(celsius) {
+  let fahrenheit = celsius * (9 / 5) + 32;
+  if (typeof fahrenheit !== "undefined") {
+    return fahrenheit;
+  } else {
+    return "fahrenheit not defined";
+  }
+}
